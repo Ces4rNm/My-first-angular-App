e52bf5650554f1b9db71886ba051d24e283e2fb9
Cesar Nuñez <alejoo2310@gmail.com>
2021-04-01 01:15:10 -0500

    version estable 2.0

--- src/app/explore-container/explore-container.component.ts
+++ src/app/explore-container/explore-container.component.ts
@@ -7,9 +7,10 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ExploreContainerComponent implements OnInit {
   @Input() name: string;
-
+  
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
 
 }
